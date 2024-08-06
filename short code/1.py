ebd8fce0cc62182a8c010695a5d1e869fed613ad
flasher14 <48672977+flasher14@users.noreply.github.com>
2024-08-06 16:34:05 +0530

    Update 1.py

--- short code/1.py
+++ short code/1.py
@@ -6,8 +6,8 @@ with open('/content/enjoysport (1).csv','r') as csvfile:
     print("csv file is: ")
     for line in lines:
         print(line)
-    if line[len(line)-1].upper()=='YES':
-        data.append(line[:-1])
+        if line[len(line)-1].upper()=='YES':
+            data.append(line[:-1])
 print("Positive eg are: ")
 for line in data:
     print(line)
