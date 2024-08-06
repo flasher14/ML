6e219c5703b3d7b3c8e6dac2acafc48cd3dc9198
flasher14 <48672977+flasher14@users.noreply.github.com>
2024-08-06 14:42:53 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -5,30 +5,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier 
 from sklearn.metrics import accuracy_score, confusion_matrix 
  
-# Load the Iris dataset 
 iris = load_iris() 
 X = iris.data 
 y = iris.target 
  
-# Split the dataset into training and testing sets 
 X_train, X_test, y_train, y_test = train_test_split(X, y, 
 test_size=0.3, random_state=42) 
  
-# Initialize the KNN classifier 
 k = 3 
 knn = KNeighborsClassifier(n_neighbors=k) 
  
-# Train the KNN classifier 
 knn.fit(X_train, y_train) 
  
-# Make predictions on the testing set 
 predictions = knn.predict(X_test) 
  
-# Calculate accuracy 
 accuracy = accuracy_score(y_test, predictions) 
 print("Accuracy:", accuracy) 
  
-# Print correct and wrong predictions 
 correct_predictions = [] 
 wrong_predictions = [] 
 for i in range(len(predictions)): 
@@ -51,7 +44,6 @@ for prediction in wrong_predictions:
 iris.target_names[prediction[1]], "Predicted Class:", 
 iris.target_names[prediction[2]]) 
  
-# Confusion Matrix 
 conf_matrix = confusion_matrix(y_test, predictions) 
 print("\nConfusion Matrix:") 
 print(conf_matrix)
