2e2d60fcedf7e6b79c7e2201af141f61689601b2
flasher14 <48672977+flasher14@users.noreply.github.com>
2024-08-06 16:18:37 +0530

    Update 1.py

--- short code/1.py
+++ short code/1.py
@@ -5,9 +5,9 @@ with open('/content/enjoysport (1).csv','r') as csvfile:
     data = []
     print("csv file is: ")
     for line in lines:
-    print(line)
+        print(line)
     if line[len(line)-1].upper()=='YES':
-    data.append(line[:-1])
+        data.append(line[:-1])
 print("Positive eg are: ")
 for line in data:
     print(line)
@@ -16,9 +16,8 @@ print(hypo)
 hypo = data[0]
 for i in range(len(data)):
     for j in range(len(data[0])):
-    if hypo[j]!=data[i][j]:
-    hypo[j] = '?'
+        if hypo[j]!=data[i][j]:
+            hypo[j] = '?'
     print(hypo)
-print("The maximally specific Find-s hypothesis for the given training 
-examples is ")
+print("The maximally specific Find-s hypothesis for the given training examples is ")
 print(hypo)
